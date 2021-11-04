335a62fcdb649567ed1f7c006a0da5ece0087c67
TANNER MECK <steve.meck246@gmail.com>
2021-11-04 14:01:03 -0700

    container created

---
+++ src/container/SearchContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchContainer extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <h1>Hello from container</h1>
+        )
+    }
+}
+export default SearchContainer;
