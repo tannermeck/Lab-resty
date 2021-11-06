4aa4d6ee26cbb8e0d2bc01f69a05e03b73fe1ae1
TANNER MECK <steve.meck246@gmail.com>
2021-11-06 13:06:21 -0700

    behavior test for container passing

---
+++ src/container/test/SearchContainer.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+import SearchContainer from '../SearchContainer';
+
+describe('it should display a todo', () => {
+    it('should list one todo by its id', async () => {
+        render(<SearchContainer />)
+        const urlInput = await screen.findByPlaceholderText('URL')
+        userEvent.type(urlInput, 'https://jsonplaceholder.typicode.com/todos/1')
+        const submit = await screen.findByRole('button');
+        userEvent.click(submit);
+        return waitFor(() => {
+            const data = screen.getAllByText('root', {exact: false})
+            expect(data).toHaveLength(1);
+        })
+        // const ul = await screen.findByRole('list')
+        // expect(ul).toMatchSnapshot();
+    })
+})
